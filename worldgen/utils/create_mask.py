eaf380670902c35264e0207755be36687df7d552
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2022-12-10 21:47:59 +0000

    good changes

--- worldgen/utils/create_mask.py
+++ worldgen/utils/create_mask.py
@@ -1,15 +1,15 @@
 from PIL import Image
 
-width, height = 1024, 1024
+width, height = 512, 512
+
+middle_mask = Image.new('RGBA', (width, height))
+black_part = Image.new('RGBA', (width // 3, height), (0, 0, 0, 255))
+middle_mask.paste(black_part, (0, 0))
+middle_mask.paste(black_part, (width // 3 * 2, 0))
+middle_mask.save('right_mask.png')
 
 
-black = Image.new('RGBA', (width // 3, height), (0, 0, 0, 255))
-
-
-# Create a new image that will be the right half and empty
-new = Image.new('RGBA', (width, height))
-
-new.paste(black, (0, 0))
-new.paste(black, (width // 3 * 2, 0))
-
-new.save('middle_mask.png')
+right_mask = Image.new('RGBA', (width, height))
+black = Image.new('RGBA', (width // 2, height), (0, 0, 0, 255))
+right_mask.paste(black, (0, 0))
+right_mask.save('right_mask.png')
