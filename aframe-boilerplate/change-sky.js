40358e5699d02d9769a39d20d6e6d292abb42517
yamz8 <yamz8>
2022-12-11 09:27:48 -0500

    add to template folder

--- aframe-boilerplate/change-sky.js
+++ aframe-boilerplate/change-sky.js
@@ -3,22 +3,17 @@ AFRAME.registerComponent('school-playground', {
       
       // Solution for Modifying Entities.
       var sceneEl = document.querySelector('a-scene'); 
-      var backgroundImages = [            
-            'https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg',
-            'https://iili.io/HnQTbpe.png',
-            'https://iili.io/HnQTDv9.png',
-            'https://iili.io/HnQTpTu.png',
-            'https://iili.io/HnQJjsa.png',
-            'https://iili.io/HnLr7bS.png',
-            'https://iili.io/HnQTyhb.png',
-            'https://iili.io/HnL4R4a.png'
-
+      var backgroundImages = [                    
+          "assets1/.jpg",
+           "assets/3.jpg"
+         ];   
         ];
         setInterval(changeBackground, 3000);                                
         let currentBackgroundIndex = 0;
       
         function changeBackground() {
           sceneEl.querySelector('a-sky').setAttribute('src', backgroundImages[currentBackgroundIndex]);  
+          console.log("ttt")
           currentBackgroundIndex++;
           if (currentBackgroundIndex >= backgroundImages.length) {
                 currentBackgroundIndex = 0;
