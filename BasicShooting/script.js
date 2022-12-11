0def2114470bb2f1b60feead1cf327369ef342f3
yamz8 <yamz8>
2022-12-10 20:10:10 -0500

    not finish

---
+++ BasicShooting/script.js
@@ -0,0 +1,65 @@
+let nextLevel = 'index.html';
+
+// create boolete and set proprties
+const shoot = () => {
+  const bullet = document.createElement("a-sphere");
+  let pos = myCamera.getAttribute("position");
+  bullet.setAttribute("position", pos);
+  bullet.setAttribute("velocity", getDirection(myCamera, 30)); //set speed 30 to the direction of cemera
+  bullet.setAttribute("dynamic-body", true);
+  bullet.setAttribute("radius", 0.5);
+  bullet.setAttribute("src", "https://i.imgur.com/H8e3Vnu.png");
+  myScene.appendChild(bullet);
+  bullet.addEventListener('collide', shootCollided); //every time the bollete collide call shoot collide callback
+};
+
+const teleport = () => {
+  console.log('Pressing teleport...');
+  const bullet = document.createElement("a-torus");
+  let pos = myCamera.getAttribute("position");
+  bullet.setAttribute("position", pos);
+  bullet.setAttribute("velocity", getDirection(myCamera, 30));
+  bullet.setAttribute("dynamic-body", true);
+  bullet.setAttribute("radius", 0.5);
+  bullet.setAttribute("arc", 360);
+  bullet.setAttribute("radius-tubular", 0.1);
+  bullet.setAttribute("rotation", { x: 90, y: 0, z: 0 });
+  bullet.setAttribute("src", "https://i.imgur.com/H8e3Vnu.png");
+  myScene.appendChild(bullet);
+  bullet.addEventListener('collide', teleportCollided);
+};
+
+const shootCollided = event => {
+  if (event.detail.body.el.id === 'floor') {
+    console.log('Hit the floor'); //when hit
+    event.detail.target.el.removeEventListener('collide', shootCollided);
+    myScene.removeChild(event.detail.target.el);
+  } else if (event.detail.body.el.className === 'target') {
+    console.log('Hit the target!');
+    event.detail.target.el.removeEventListener('collide', shootCollided);
+    myScene.removeChild(event.detail.target.el);
+    myScene.removeChild(event.detail.body.el);
+  }
+  if (document.querySelectorAll('.target').length === 0) {
+    console.log('You win!');
+    //location.href = nextLevel;
+  }
+};
+
+const teleportCollided = event => {
+  if (event.detail.body.el.id === 'floor') {
+    console.log('Hit the floor');
+    event.detail.target.el.removeEventListener('collide', teleportCollided);
+    let pos = event.detail.target.el.getAttribute('position');
+    myScene.removeChild(event.detail.target.el);
+    myCamera.setAttribute('position', { x: pos.x, y: 2, z: pos.z });
+  }
+};
+
+document.onkeydown = event => {
+  if (event.which == 32) {
+    shoot();
+  } else if (event.which == 67) {
+    teleport();
+  }
+};
